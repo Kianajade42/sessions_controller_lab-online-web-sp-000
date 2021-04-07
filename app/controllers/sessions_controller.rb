c0ca6a7d1370c6722fde2cba851214cd89822efd
Kiana Long <kianajade42.kl@gmail.com>
2021-04-07 20:12:59 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete :username
+    session.delete :name
     redirect_to controller: 'application', action: 'hello'
     end
 end
